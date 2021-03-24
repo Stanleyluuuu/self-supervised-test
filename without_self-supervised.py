666f9d5a34e9b48ed2ba102f3769f977ee70c164
stanleyluuuu <stanley860920@gmail.com>
2021-03-24 17:40:04 +0800

    testing without self-supervised

---
+++ without_self-supervised.py
@@ -0,0 +1,120 @@
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import pdb
+import wandb
+from PIL import Image
+
+train_dir = "/media/public_dataset/ImageNet_200"
+val_dir = "/media/public_dataset/ImageNet/imagenet/ILSVRC2012_img_val"
+val_gt_dir = "/media/public_dataset/ImageNet/imagenet/val.txt"
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+sweep_config = {
+    'method': 'random', #grid, random
+    'metric': {
+      'name': 'loss',
+      'goal': 'minimize'   
+    },
+    'parameters': {
+        'epochs': {
+            'values': [1, 100]
+        },
+        'batch_size': {
+            'values': [80]
+        },
+        'learning_rate': {
+            'values': [1e-3]
+        },
+        'optimizer': {
+            'values': ['adam', 'sgd']
+        },
+    }
+}
+sweep_id = wandb.sweep(sweep_config, project="without self_sup testing")
+
+config_defaults = {
+        'epochs': 1,
+        'batch_size': 80,
+        'learning_rate': 1e-3,
+        'optimizer': 'adam'
+    }
+wandb.init(config=config_defaults)
+config = wandb.config
+
+########################################
+# Getting training set into dataloader #
+########################################
+print("Start loading training data!")
+trainset = torchvision.datasets.ImageFolder(root=train_dir, transform=preprocess)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=config.batch_size, shuffle=True)
+print("Finish loading training data!")
+
+######################################################
+# Getting validation set into dataloader as test set #
+######################################################
+print("Start loading validation data!") # get the image name which belongs to class 1-200
+val_x = []
+val_y = []
+f = open(val_gt_dir, "r") 
+for d in f:
+    if int(d[29:]) <= 200:
+        image_name = d[:28]
+        path = os.path.join(val_dir, image_name)
+        valid = Image.open(path)
+        if len(np.array(valid).shape) != 3:
+            valid = valid.convert("RGB")
+        valid_tensor = preprocess(valid)
+        val_x.append(np.array(valid_tensor))
+        val_y.append(int(d[29:]))
+f.close()
+
+tensor_val_x, tensor_val_y = torch.Tensor(val_x), torch.Tensor(val_y)
+validset = torch.utils.data.TensorDataset(tensor_val_x, tensor_val_y)
+print("Finish loading validation data!")
+# pdb.set_trace()
+
+resnet50 = torchvision.models.resnet50()
+
+criterion = nn.CrossEntropyLoss()
+if config.optimizer=='sgd':
+    optimizer = optim.SGD(resnet50.parameters(),lr=config.learning_rate, momentum=0.9)
+elif config.optimizer=='adam':
+    optimizer = optim.Adam(resnet50.parameters(),lr=config.learning_rate, betas=(0.9, 0.999))
+
+# use GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+resnet50.to(device)
+print(device)
+print("Start training!")
+for epoch in range(config.epochs):
+    closs = 0
+    for i, data in enumerate(trainloader, 0):
+        # get input and labels
+        inputs, labels = data[0].to(device), data[1].to(device)
+        # zero the gradient
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = resnet50(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # calculate the loss
+        closs += loss.item()
+        wandb.log({"training batch loss":loss.item()})
+    wandb.log({"training loss":closs/config.batch_size})
+    print('epoch %d loss: %.3f' % (epoch + 1, closs / 5200))
+print("Finish training!")
+pdb.set_trace()
